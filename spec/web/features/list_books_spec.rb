4f19eb7329b6a6bfe1610190e8b6076c06de1ac7
Orban Botond <orbanbotond@gmail.com>
2018-08-29 00:13:46 +0300

    First Hanami app

---
+++ spec/web/features/list_books_spec.rb
@@ -0,0 +1,12 @@
+# spec/web/features/list_books_spec.rb
+require 'features_helper'
+
+describe 'List books' do
+  it 'displays each book on the page' do
+    visit '/books'
+
+    within '#books' do
+      assert page.has_css?('.book', count: 2), 'Expected to find 2 books'
+    end
+  end
+end
