46d16485a52258717c02afba909beaef2f1cf970
Orban Botond <orbanbotond@gmail.com>
2018-09-02 18:25:11 +0300

    The first queries are in the repository object

--- lib/bookshelf/repositories/book_repository.rb
+++ lib/bookshelf/repositories/book_repository.rb
@@ -1,2 +1,12 @@
 class BookRepository < Hanami::Repository
+  def most_recent_by_author(author, limit: 8)
+    books
+      .where(author: author)
+      .order(:created_at)
+      .limit(limit)
+  end
+
+  def count
+    books.count
+  end
 end
